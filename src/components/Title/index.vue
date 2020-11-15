ca05a88c7b805a8ca6a19850252288c3cf29d796
Radmir Mirhaydarov <Mirhaydarov@protonmail.com>
2020-11-15 18:42:35 +0500

    feat: add Title component

---
+++ src/components/Title/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <h1 class="title">{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Simon Says',
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+.title {
+  margin: 1em 0 2em;
+  text-align: center;
+}
+
+</style>
