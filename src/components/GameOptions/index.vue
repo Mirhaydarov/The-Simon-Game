c1560eef79fb88c3e19c49d5d4b1c797e0c6ab14
Radmir Mirhaydarov <Mirhaydarov@protonmail.com>
2020-11-15 18:46:54 +0500

    feat: add GameOptions component

---
+++ src/components/GameOptions/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="game-options">
+    <h2>{{ title }}</h2>
+    <input type="radio" value="normal" v-model="inputList">
+      Normal
+    <br>
+    <input type="radio" value="middle" v-model="inputList">
+      Middle
+    <br>
+    <input type="radio" value="hard" v-model="inputList">
+      Hard
+    <br>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Game Options:',
+      inputList: 'normal',
+    };
+  },
+
+  updated() {
+    this.$emit('difficult', this.inputList);
+  },
+};
+</script>
