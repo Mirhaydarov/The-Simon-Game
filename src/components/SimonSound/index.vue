34a674cd007165b676391b7916214425e6bc1ec1
Radmir Mirhaydarov <Mirhaydarov@protonmail.com>
2020-11-15 18:44:48 +0500

    feat: add SimonSound component

---
+++ src/components/SimonSound/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer data-action="sound" ref="soundList">
+    <audio
+      src="../../sounds/1.mp3"
+      data-sound="red"
+    ></audio>
+    <audio
+      src="../../sounds/2.mp3"
+      data-sound="green"
+    ></audio>
+    <audio
+      src="../../sounds/3.mp3"
+      data-sound="blue"
+    ></audio>
+    <audio
+      src="../../sounds/4.mp3"
+      data-sound="yellow"
+    ></audio>
+  </footer>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.$emit('soundList', this.$refs.soundList);
+  },
+};
+</script>
