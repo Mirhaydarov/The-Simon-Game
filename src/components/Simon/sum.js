98fd56ab6429c8adea2698535528d17a311272d0
Radmir Mirhaydarov <Mirhaydarov@protonmail.com>
2020-11-15 19:37:10 +0500

    feat: add sum()
    sum() will take difficult' value multiply it to number and return sum.
    Result will use for calculate time when a tiles should be to light up.
    And when user can click on the tiles.

---
+++ src/components/Simon/sum.js
@@ -0,0 +1,4 @@
+import { typeOfDifficult } from './type-of-difficult';
+
+// eslint-disable-next-line import/prefer-default-export
+export const sum = (difficult, num) => typeOfDifficult(difficult) * num;
