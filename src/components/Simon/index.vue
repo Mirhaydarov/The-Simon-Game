4aad83ae782f087e3077e2385e7144ed29886809
Radmir Mirhaydarov <Mirhaydarov@protonmail.com>
2020-11-15 18:52:51 +0500

    feat: add Simon component

---
+++ src/components/Simon/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="simon">
+    <ul ref="tileList">
+      <li class="red" data-tile="red"></li>
+      <li class="blue" data-tile="blue"></li>
+      <li class="yellow" data-tile="yellow"></li>
+      <li class="green" data-tile="green"></li>
+    </ul>
+  </main>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang="scss" scoped>
+
+.red, .green, .blue, .yellow {
+  cursor: pointer;
+}
+
+</style>
