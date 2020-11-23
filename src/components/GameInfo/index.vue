db4bd53f0687a93d6c7fce34a413b9cc4d901596
Radmir Mirhaydarov <Mirhaydarov@protonmail.com>
2020-11-23 19:09:23 +0500

    refactor: change emit to eventEmitter in GameInfo
    I did this so that the parent does not keep an extra state in himself.
    Also added method, for start game and state for show or hide a start button, and info message.
    Now vue decide when show or hide a component instead a class.

--- src/components/GameInfo/index.vue
+++ src/components/GameInfo/index.vue
@@ -1,27 +1,52 @@
 <template>
   <section class="game-info">
     <h2>Round: <span data-round="0">{{ score }}</span></h2>
-    <button class="start" ref="startBtn" @click="startGame">Start</button>
-    <span class="info hidden" ref="info"></span>
+    <button
+      v-show="showStartBtn"
+      class="start" ref="startBtn"
+      @click="startGame"
+    >Start</button>
+    <span
+      v-show="showInfo"
+      class="info"
+      ref="info"
+    >{{ infoMessage }}</span>
   </section>
 </template>
 
 <script>
+import { eventEmitter } from '../../main';
+
 export default {
   props: {
     score: {
       type: Number,
       required: true,
     },
-    startGame: {
-      type: Function,
-      default: () => {},
+  },
+
+  data() {
+    return {
+      showStartBtn: true,
+      showInfo: false,
+      infoMessage: '',
+    };
+  },
+
+  methods: {
+    startGame() {
+      eventEmitter.$emit('startGames');
     },
   },
 
-  mounted() {
-    this.$emit('startBtn', this.$refs.startBtn);
-    this.$emit('info', this.$refs.info);
+  created() {
+    eventEmitter.$on('hideStartBtn', () => {
+      this.showStartBtn = !this.showStartBtn;
+    });
+    eventEmitter.$on('showInfo', (payload) => {
+      this.showInfo = !this.showInfo;
+      this.infoMessage = (this.showInfo) ? payload : '';
+    });
   },
 };
 </script>
