2f9c77ccdf718486174c9ac24c91c2ea18eae135
Radmir Mirhaydarov <Mirhaydarov@protonmail.com>
2020-11-24 09:29:52 +0500

    refactor: remove unuse import

--- src/components/Simon/filter-sound.js
+++
@@ -1,6 +0,0 @@
-export const filterSound = (soundList, value) => {
-  const copySoundList = [...soundList.childNodes];
-
-  const currentSound = copySoundList.filter((sound) => sound.getAttribute('data-sound') === value);
-  return currentSound[0];
-};
